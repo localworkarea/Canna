ff7f6d221eabb8322f4da6ce0602bf56ed82b2e3
Denys Mykhailenko <localworkarea@gmail.com>
2023-01-19 22:46:43 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -266,74 +266,74 @@ sideClose();
 tabs();
 
 
-	if (document.querySelector('.filters__slider')) { 
-		// Створюємо слайдер
-		new Swiper('.filters__slider', {
-			slidesPerView: 10,
-			// spaceBetween: 0,
-			// autoHeight: true,
-			// speed: 800,
-			//touchRatio: 0,
-			//simulateTouch: false,
-			// loop: true,
-			//preloadImages: false,
-			//lazy: true,
-			/*
-			// Ефектиnpm run
-			effect: 'fade',
-			autoplay: {
-				delay: 3000,
-				disableOnInteraction: false,
-			},
-			*/
+	// if (document.querySelector('.filters__slider')) { 
+	// 	// Створюємо слайдер
+	// 	new Swiper('.filters__slider', {
+	// 		slidesPerView: 10,
+	// 		// spaceBetween: 0,
+	// 		// autoHeight: true,
+	// 		// speed: 800,
+	// 		//touchRatio: 0,
+	// 		//simulateTouch: false,
+	// 		// loop: true,
+	// 		//preloadImages: false,
+	// 		//lazy: true,
+	// 		/*
+	// 		// Ефектиnpm run
+	// 		effect: 'fade',
+	// 		autoplay: {
+	// 			delay: 3000,
+	// 			disableOnInteraction: false,
+	// 		},
+	// 		*/
 
-			// Пагінація
-			/*
-			pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-			},
-			*/
+	// 		// Пагінація
+	// 		/*
+	// 		pagination: {
+	// 			el: '.swiper-pagination',
+	// 			clickable: true,
+	// 		},
+	// 		*/
 
-			// Скроллбар
-			scrollbar: {
-				el: '.swiper-scrollbar',
-				draggable: true,
-			},
+	// 		// Скроллбар
+	// 		scrollbar: {
+	// 			el: '.swiper-scrollbar',
+	// 			draggable: true,
+	// 		},
 
-			// Кнопки "вліво/вправо"
-			// navigation: {
-			// 	prevEl: '.filters__slider .swiper-button-prev',
-			// 	nextEl: '.filters__slider .swiper-button-next',
-			// },
-			/*
-			// Брейкпоінти
-			breakpoints: {
-				640: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
-				},
-				768: {
-					slidesPerView: 2,
-					spaceBetween: 20,
-				},
-				992: {
-					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 4,
-					spaceBetween: 30,
-				},
-			},
-			*/
-			// Події
-			on: {
+	// 		// Кнопки "вліво/вправо"
+	// 		// navigation: {
+	// 		// 	prevEl: '.filters__slider .swiper-button-prev',
+	// 		// 	nextEl: '.filters__slider .swiper-button-next',
+	// 		// },
+	// 		/*
+	// 		// Брейкпоінти
+	// 		breakpoints: {
+	// 			640: {
+	// 				slidesPerView: 1,
+	// 				spaceBetween: 0,
+	// 				autoHeight: true,
+	// 			},
+	// 			768: {
+	// 				slidesPerView: 2,
+	// 				spaceBetween: 20,
+	// 			},
+	// 			992: {
+	// 				slidesPerView: 3,
+	// 				spaceBetween: 20,
+	// 			},
+	// 			1268: {
+	// 				slidesPerView: 4,
+	// 				spaceBetween: 30,
+	// 			},
+	// 		},
+	// 		*/
+	// 		// Події
+	// 		on: {
 
-			}
-		});
-	}
+	// 		}
+	// 	});
+	// }
   
 	if (document.querySelector('.product-item__slider')) { 
 		// Створюємо слайдер
